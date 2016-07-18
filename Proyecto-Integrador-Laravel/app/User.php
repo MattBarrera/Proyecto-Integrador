ad2d773beedbb761206396de197f1cacf2219748
manuelvilche <ManuelVilche@MacBook-Pro-de-Manuel.local>
2016-07-18 16:32:46 -0300

    Create user

--- Proyecto-Integrador-Laravel/app/User.php
+++ Proyecto-Integrador-Laravel/app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name','lastname','phone', 'email','gender','avatar','birthdate', 'password','status',
     ];
 
     /**
