0d92b6cf3cd253c6e43acb6d5cf0fecb1c2c60fc
MattBarrera <mb_herfarth@hotmail.com>
2016-08-08 19:12:43 -0300

    vistas

---
+++ Proyecto-Integrador-Laravel/resources/views/nuevoGenero.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<form>
+    <div class="form-group">
+    <label>Nuevo Genero</label>
+    <input type="text" class="form-control" placeholder="Ingrese nuevo genero">
+  </div>
+</form>
+@endsection
